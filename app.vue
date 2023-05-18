4bc1d6fc314f3c31c4b7482196130331b3d7bd76
Zach Kusz <zack.kusz@capitollien.com>
2023-05-18 10:44:31 -0500

    improve title visibility

--- app.vue
+++ app.vue
@@ -19,9 +19,11 @@ const heroSet = setsListData.value.find((set) => {
   <v-app>
     <v-app-bar :image="heroSet.photos[heroSet.heroPhotoIndex].link" absolute>
       <template v-slot:image>
-        <v-img :alt="heroSet.name" align-end></v-img>
+        <v-img :alt="heroSet.name" align-end class="header-image"></v-img>
       </template>
-      <v-app-bar-title><NuxtLink to="/">Deconstructed Lego</NuxtLink></v-app-bar-title>
+      <v-app-bar-title>
+        <NuxtLink to="/">Deconstructed Lego</NuxtLink>
+      </v-app-bar-title>
     </v-app-bar>
     <v-main>
       <NuxtPage />
@@ -31,10 +33,13 @@ const heroSet = setsListData.value.find((set) => {
 </template>
 
 <style>
-.v-app-bar-title a {
-  text-decoration: none;
-  text-shadow: 0px 0px 10px #000000, 0px 0px 10px #000000;
-  color: white;
+.header-image {
+  filter: contrast(1.2) sepia(0.3);
 }
 
+.v-app-bar-title a {
+  text-shadow: 0px 0px 10px #000000, 0px 0px 10px #000000;
+  color: white;
+  font-weight: bold;
+}
 </style>
